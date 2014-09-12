65f24a957f7329942f90559e4ae28b1542c26eff
cemolcay <ccemolcay@gmail.com>
2014-09-12 18:07:02 +0300

    Initial Commit

---
+++ StrechyParallaxScrollView/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  StrechyParallaxScrollView
+//
+//  Created by Cem Olcay on 12/09/14.
+//  Copyright (c) 2014 questa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
